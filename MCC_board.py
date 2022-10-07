ee4fa3aa26782ae2d167ecf68565eb85e41045d4
Artur S <artur.schneider@biologie.uni-freiburg.de>
2022-10-07 17:54:43 +0200

    .

--- MCC_board.py
+++ MCC_board.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, division, print_function
 from builtins import *  # @UnusedWildImport
 from time import sleep
+import datetime 
+
 from ctypes import cast, POINTER, c_double, c_ushort, c_ulong ,addressof, sizeof
 
 from mcculw import ul
@@ -10,23 +12,44 @@ from mcculw.ul import ULError
 
 from mcc_utils import config_first_detected_device
 
+class RecordingConfig:
+    def __init__(self):
+        self.session_name = "TestSession"
+        self.subject = "MusterSubject"
+        self.session_path = ''
+        self.VERSION = 0.1
+        self.sampling_rate = 100 
+        self.board_num = 0
+        self.dev_id_list = [276]
+        
+#TODO find out sizes which are returned from the background process
+#TODO test writing/reading to a binary datafile 
+
 
 class MCCBoard:
-    '''Class for acquiring data from a MCC board on a host computer.'''
-    def __init__(self,board_num=0,sampling_rate = 100):
-        self.ai_info = None
-        self.dio_info = None
-        self.sampling_rate = sampling_rate # Hz
-
-        self.board_num = board_num
-        self.dev_id_list = [276]
+    '''Class for acquiring data from a MCC board on a host computer.
+    This class may be reused in different gui applications, thus should be a self_sufficent container    
+    '''
+    def __init__(self,cfg,file_type='csv'):
+        
+        self.cfg = cfg # this shall receive a cfg dict with all the variables.. which are read by GUI from a config file.        
+        self.data_temp = './' # here we temporally record the data, afterwards it could be moved to corresponding server ? Abstract the move to GUI
+        
+        self.sampling_rate = self.cfg.sampling_rate # Hz
+        self.board_num = self.cfg.board_num
+        self.dev_id_list = self.cfg.dev_id_list
+        self.file_type = file_type
+        
         self.ai_range = ULRange.BIP10VOLTS
         self.connected = False
 
-        self.memhandle = None
+        self.memhandle = None        
+        self.ai_info = None
+        self.dio_info = None
+        
         self.data_file = None
         self.running = False
-        self.file_type = None
+        
 
     def connect(self):
         try:
@@ -45,6 +68,7 @@ class MCCBoard:
 
             print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
                   daq_dev_info.unique_id, ')\n', sep='')
+            self.cfg.daq_device = daq_dev_info.product_name
             self.ai_info = daq_dev_info.get_ai_info()
             self.dio_info = daq_dev_info.get_dio_info()
 
@@ -61,6 +85,9 @@ class MCCBoard:
                   + " Message: " + e.message)
 
     def read_singlevalue(self,channel=0):
+        """
+        Just a short test fucntion to be deleted later
+        """
         try:
             if self.ai_info.resolution <= 16:
                 # Use the a_in method for devices with a resolution <= 16
@@ -85,6 +112,9 @@ class MCCBoard:
             print('\n', e)
 
     def read_multiplevalue(self, points_per_channel = 1000):
+        """
+        Just a short test fucntion to be deleted later
+        """
         low_chan = 0
         high_chan = 7
         num_chans = high_chan - low_chan + 1
@@ -160,8 +190,12 @@ class MCCBoard:
                 # Free the buffer in a finally block to prevent a memory leak.
                 ul.win_buf_free(self.memhandle)
                 self.memhandle = None
-
+    
+   
     def record_infile(self):
+        """
+        test function records a predefined length of samples into a file
+        """
         low_chan = 0
         high_chan = 3
         num_chans = high_chan - low_chan + 1
@@ -329,11 +363,144 @@ class MCCBoard:
                 self.memhandle = None
 
     def disconnect(self):
+        self.stop_recording() # just in case
         ul.release_daq_device(self.board_num)
-        self.connected = False
+        self.connected = False        
         if self.memhandle:
             # Free the buffer in a finally block to prevent a memory leak.
             ul.win_buf_free(self.memhandle)
+    
+    def start_recording(self):        
+        '''Open data file and write data header.'''
+        assert self.file_type in ['csv', 'ppd'], 'Invalid file type'
+        #self.file_type = file_type
+        date_time = datetime.now()
+        file_name = subject_ID + date_time.strftime('-%Y-%m-%d-%H%M%S') + '.' + file_type
+        file_path = os.path.join(self.data_dir, file_name)
+        header_dict = {'subject_ID': self.cfg.subject,
+                       'session': self.cfg.session_name,
+                       'date_time' : date_time.isoformat(timespec='seconds'),
+                       'daq_device': self.cfg.daq_device,
+                       'sampling_rate': self.sampling_rate,                       
+                       'version': self.cfg.VERSION}
+        #TODO add info about channels we r recording!
+        #Check the other config i had maybe can be reused !
+        if file_type == 'ppd': # Single binary .ppd file.
+            self.data_file = open(file_path, 'wb')
+            data_header = json.dumps(header_dict).encode()
+            self.data_file.write(len(data_header).to_bytes(2, 'little'))
+            self.data_file.write(data_header)
+        elif file_type == 'csv': # Header in .json file and data in .csv file.
+            with open(os.path.join(data_dir, file_name[:-4] + '.json'), 'w') as headerfile:
+                headerfile.write(json.dumps(header_dict, sort_keys=True, indent=4))
+            self.data_file = open(file_path, 'w')
+            self.data_file.write('Analog1, Analog2, Digital1, Digital2\n')
+        return file_name
+        
+    def stop_recording(self):
+        if self.data_file:
+            self.data_file.close()
+        self.data_file = None
+    
+    def grab_and_dump(self):
+        """
+        here i grab a piece of data write it to file and return it to the caller
+        - should be analogous to process_data() of acquisition_board
+        so far only copied not run or tested 
+        
+        only dump if recording was started, otherwise just return values for display
+        """
+       
+        # Start the write loop
+        prev_count = 0
+        prev_index = 0
+        write_ch_num = low_chan
+        while status != Status.IDLE:
+            # Get the latest counts
+            status, curr_count, _ = ul.get_status(self.board_num,
+                                                  FunctionType.AIFUNCTION)
 
+            new_data_count = curr_count - prev_count
 
+            # Check for a buffer overrun before copying the data, so
+            # that no attempts are made to copy more than a full buffer
+            # of data
+            if new_data_count > ul_buffer_count:
+                # Print an error and stop writing
+                ul.stop_background(self.board_num, FunctionType.AIFUNCTION)
+                print('A buffer overrun occurred')
+                break
+
+            # Check if a chunk is available
+            if new_data_count > write_chunk_size:
+                wrote_chunk = True
+                # Copy the current data to a new array
+
+                # Check if the data wraps around the end of the UL
+                # buffer. Multiple copy operations will be required.
+                if prev_index + write_chunk_size > ul_buffer_count - 1:
+                    first_chunk_size = ul_buffer_count - prev_index
+                    second_chunk_size = (
+                            write_chunk_size - first_chunk_size)
+
+                    # Copy the first chunk of data to the
+                    # write_chunk_array
+                    ul.scaled_win_buf_to_array(
+                        self.memhandle, write_chunk_array, prev_index,
+                        first_chunk_size)
+
+                    # Create a pointer to the location in
+                    # write_chunk_array where we want to copy the
+                    # remaining data
+                    second_chunk_pointer = cast(addressof(write_chunk_array)
+                                                + first_chunk_size
+                                                * sizeof(c_double),
+                                                POINTER(c_double))
+
+                    # Copy the second chunk of data to the
+                    # write_chunk_array
+                    ul.scaled_win_buf_to_array(
+                        self.memhandle, second_chunk_pointer,
+                        0, second_chunk_size)
+                else:
+                    # Copy the data to the write_chunk_array
+                    ul.scaled_win_buf_to_array(
+                        self.memhandle, write_chunk_array, prev_index,
+                        write_chunk_size)
+
+                # Check for a buffer overrun just after copying the data
+                # from the UL buffer. This will ensure that the data was
+                # not overwritten in the UL buffer before the copy was
+                # completed. This should be done before writing to the
+                # file, so that corrupt data does not end up in it.
+                status, curr_count, _ = ul.get_status(
+                    self.board_num, FunctionType.AIFUNCTION)
+                if curr_count - prev_count > ul_buffer_count:
+                    # Print an error and stop writing
+                    ul.stop_background(self.board_num, FunctionType.AIFUNCTION)
+                    print('A buffer overrun occurred')
+                    break
+                if self.data_file:
+                    for i in range(write_chunk_size):
+                        self.data_file.write(str(write_chunk_array[i]) + ',')
+                        write_ch_num += 1
+                        if write_ch_num == high_chan + 1:
+                            write_ch_num = low_chan
+                            self.data_file.write(u'\n')
+            else:
+                wrote_chunk = False
+
+            if wrote_chunk:
+                # Increment prev_count by the chunk size
+                prev_count += write_chunk_size
+                # Increment prev_index by the chunk size
+                prev_index += write_chunk_size
+                # Wrap prev_index to the size of the UL buffer
+                prev_index %= ul_buffer_count
+                
+            else:
+                # Wait a short amount of time for more data to be
+                # acquired.
+                sleep(0.1)
+    
 
