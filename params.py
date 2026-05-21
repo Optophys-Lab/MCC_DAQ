ea9c2b8407388da269feddcef51bff9ee31c2e02
ZPJ1000 <99723794+ZPJ1000@users.noreply.github.com>
2026-05-21 16:44:45 +0200

    Changed data path
    diff save path

--- params.py
+++ params.py
@@ -2,4 +2,4 @@ HOST = "localhost"  # if connecting to remote, use the IP of the current machine
 PORT = 8800
 ENABLE_REMOTE = True
 DAQ_FOLDER = 'daq'
-DEFAULT_SAVE_PATH = "data"
+DEFAULT_SAVE_PATH = r"O:\archive\projects\2022_optoroborat\misc\ZoeDebug\TestRecordingsCamera"
